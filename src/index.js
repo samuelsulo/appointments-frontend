b18c6756836cdabfdf5a0c5b9999dafb99155f25
Samuel Sulo <samuelsulo0802@gmail.com>
2021-06-25 00:54:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 const store = createStore(reducers,  compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(),
     ));
 
 ReactDOM.render(
